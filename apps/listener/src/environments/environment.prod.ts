d5c86dc9635d8ea31e3ed01a61c8319c811123ad
Nikolai Evseev <evseevnn@gmail.com>
2022-07-01 12:04:40 +0100

    [LISTENER] Update config

--- apps/listener/src/environments/environment.prod.ts
+++ apps/listener/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   MONGODB_URL: process.env.MONGODB_URL,
+  ETHEREUM_NETWORK: process.env.ETHEREUM_NETWORK,
+  INFURA_KEY: process.env.INFURA_KEY,
 };
--- apps/listener/src/environments/environment.ts
+++ apps/listener/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   MONGODB_URL: process.env.MONGODB_URL,
-  ETHEREUM_NETWORK: 'rinkeby',
+  ETHEREUM_NETWORK: process.env.ETHEREUM_NETWORK,
   INFURA_KEY: process.env.INFURA_KEY,
 };
